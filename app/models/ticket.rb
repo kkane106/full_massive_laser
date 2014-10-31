b80e693c45fdc737014da0173eccde34cd304fb0
kkane106 <kkane106@gmail.com>
2014-10-31 18:53:59 -0400

    Add beginning of show user/pro Add some work on user/pro partials Add method for claimed? in ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -3,3 +3,10 @@ class Ticket < ActiveRecord::Base
 	belongs_to :professional
 	validates :title, :body, presence: true
 end
+
+def claimed?
+	if self.professional
+		return "Claimed" 
+	else
+		"Unclaimed"
+end
