e4e8135caa39bb902e93444afb1a404fec5a1705
kkane106 <kkane106@gmail.com>
2014-10-31 10:57:19 -0400

    Add ability to create user -register (user create) partial -updated user routes in controller -added partial to the user new view -validations for presence of user fields and ticket fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,32 @@ class UsersController < ApplicationController
   end
 
   def show
-  end
-
-  def create
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Your account has been created successfully"
+      redirect_to @user
+    else
+      flash[:error] = "Please fill in all fields"
+      redirect_to 'new'
+    end
+  end
+
+
   def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
 
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,6 @@
 class Ticket < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :professional, class_name: "Proficiency"
+
+	validates :title, :body, presence: true
 end
