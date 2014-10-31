419f1ca7bfdcf989218392ffd26d6cfae1b99da0
kkane106 <kkane106@gmail.com>
2014-10-31 17:05:02 -0400

    Add validation and association tests for: -ticket model -profession model -professional model -user model

--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,5 +1,9 @@
 class Profession < ActiveRecord::Base
 	has_many :professionals
 	has_many :users
+
+	validates :name, presence: true
+	validates :name, uniqueness: true
+
 	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
 	has_many :professionals
 
 	validates :name, :email, :password, presence: true
+	validates :email, uniqueness: true
 	validates :password, length: { minimum: 4 }
 	has_secure_password
 	def pro?
