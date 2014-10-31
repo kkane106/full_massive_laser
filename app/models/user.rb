14ce3020c49c6d75e152615a17d3fd7e0955c777
kkane106 <kkane106@gmail.com>
2014-10-31 16:39:10 -0400

    add validation tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
 	has_many :professionals
 
 	validates :name, :email, :password, presence: true
-
+	validates :password, length: { minimum: 4 }
 	has_secure_password
 	def pro?
 		user = User.find(session[:user_id])
