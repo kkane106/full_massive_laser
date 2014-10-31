dac79f7d91116bb9e7696e01e8865acbbd98248f
kkane106 <kkane106@gmail.com>
2014-10-30 21:36:24 -0400

    Add controllers with actions in comments for all known models

---
+++ app/controllers/professions_controller.rb
@@ -0,0 +1,30 @@
+class ProfessionsController < ApplicationController
+
+	# def index
+	# end
+
+	# def show
+	# end
+
+	# def new
+	# end
+
+	# def create
+	# end
+
+	# def edit
+	# end
+
+	# def update
+	# end
+
+	# def destroy
+	# end
+
+	# private
+
+	# def profession_params
+	# 	params.require().permit()
+	# end
+
+end
