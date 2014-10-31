b7d0c99591a4a312095ca2f3f50731e5e9c674c2
Sean Ellis <seanellis@Seans-MacBook-Pro.local>
2014-10-30 21:09:46 -0400

    user, profession and appointment models and migrations added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'yaml'
+
+path = Rails.root.join('db/seed_data.yaml')
+seed_data = YAML.load File.read path
+
+users = seed_data[:users]
+professions = seed_data[:professions]
+appointments = seed_data[:appointments]
+
+users.each do |attrs|
+  User.new(attrs).save(:validate => false)
+end
+
+professions.each do |attrs|
+  Profession.new(attrs).save(:validate => false)
+end
+
+appointments.each do |attrs|
+  Appoint.new(attrs).save(:validate => false)
+end
